90f95643388e466da25c87ccc5ec9ba4f7d43381
Julian Seeger <seeger.julian@gmail.com>
2016-06-01 16:41:04 +0200

    Update snap-init.sh

--- snap-init.sh
+++ snap-init.sh
@@ -41,14 +41,13 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
     
     if [ ! -d /opt/android-ndk-r11c ]; then
         if [ -d ${SNAP_CACHE_DIR}/android-ndk-r11c ]; then
-            cp -r ${SNAP_CACHE_DIR}/android-ndk-r11c /opt/android-ndk-r11c
+            cp -r ${SNAP_CACHE_DIR}/android-ndk-r11c ~/android-ndk-r11c
         else
             wget http://dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip -O ndk.zip
             unzip ndk.zip
-            mv android-ndk-r11c /opt/android-ndk-r11c
             cp -r /opt/android-ndk-r11c ${SNAP_CACHE_DIR}/android-ndk-r11c
         fi
     fi
-    export PATH=${PATH}:/opt/android-ndk-r11c
+    export PATH=${PATH}:~/android-ndk-r11c
     touch ${INITIALIZATION_FILE}
 fi
