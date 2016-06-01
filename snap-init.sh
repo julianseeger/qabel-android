8607764ba19ea531e7f7bc75c20d0c34d1373fe3
Julian Seeger <seeger.julian@gmail.com>
2016-06-01 22:36:03 +0200

    Update snap-init.sh

--- snap-init.sh
+++ snap-init.sh
@@ -19,7 +19,7 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
         echo y | android update sdk --no-ui --filter tool > /dev/null
       
         # The BuildTools version used by your project
-        echo y | android update sdk --no-ui --filter build-tools-23.0.3 --all > /dev/null
+        echo y | android update sdk --no-ui --filter build-tools-23.0.1 --all > /dev/null
       
         # The SDK version used to compile your project
         echo y | android update sdk --no-ui --filter android-23 > /dev/null
